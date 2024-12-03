ed765ca59b527402719448620e9a2b0e628fc1c7
Mislav Češnik <mislav.cesnik88@gmail.com>
2024-12-03 17:10:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<html>
+<head>
+    <title>Registration Form</title>
+    <style type="text/css">
+        body {
+            background-color: #fff;
+            border-top: solid 10px #000;
+            color: #333;
+            font-size: .85em;
+            margin: 20px;
+            padding: 20px;
+            font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+        }
+        h1, h2, h3 {
+            color: #000;
+            margin-bottom: 0;
+            padding-bottom: 0;
+        }
+        h1 {
+            font-size: 2em;
+        }
+        h2 {
+            font-size: 1.75em;
+        }
+        h3 {
+            font-size: 1.2em;
+        }
+        table {
+            margin-top: 0.75em;
+        }
+        th {
+            font-size: 1.2em;
+            text-align: left;
+            border: none;
+            padding-left: 0;
+        }
+        td {
+            padding: 0.25em 2em 0.25em 0;
+            border: 0 none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Register here!</h1>
+    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+    <form method="post" action="index.php" enctype="multipart/form-data">
+        Name
+        <input type="text" name="name" id="name" />
+        <br> Email
+        <input type="text" name="email" id="email" />
+        <br>
+        <input type="submit" name="submit" value="Submit" />
+    </form>
+    <?php
+    // DB connection info
+    $host = "mcesnik-lv3-ruap-server.mysql.database.azure.com";
+    $user = "ddthbfxrbf";
+    $pwd = "9N0rmGuGaqUv6a1$";
+    $db = "mcesnik-lv3-ruap-database";
+
+    // Connect to database
+    $conn = mysqli_connect($host, $user, $pwd, $db);
+    if (mysqli_connect_errno()) {
+        echo "<h3>Failed to connect to MySQL:</h3> " . mysqli_connect_error();
+    }
+
+    // Insert registration info
+    if (!empty($_POST)) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $date = date("Y-m-d");
+
+        // Insert data
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date) VALUES ('$name','$email','$date')";
+        if ($conn->query($sql_insert) === TRUE) {
+            echo "<h3>You're registered!</h3>";
+
+            // Retrieve data
+            $sql_select = "SELECT * FROM registration_tbl";
+            $registrants = $conn->query($sql_select);
+
+            if ($registrants->num_rows > 0) {
+                echo "<h2>People who are registered:</h2>";
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Date</th></tr>";
+
+                while ($registrant = $registrants->fetch_assoc()) {
+                    echo "<tr><td>" . $registrant['name'] . "</td>";
+                    echo "<td>" . $registrant['email'] . "</td>";
+                    echo "<td>" . $registrant['date'] . "</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } else {
+            echo "Insert Failed";
+        }
+    }
+    ?>
+</body>
+</html>
